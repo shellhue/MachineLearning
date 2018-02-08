4c792ed2b73ef01c713797c2722bebd95607774f
shellhue <zeyu_huang@163.com>
2018-02-09 07:31:47 +0800

    梯度校验

--- fc.py
+++ fc.py
@@ -2,7 +2,7 @@ import numpy as np
 from functools import reduce
 
 class FullConnectedLayer(object):
-    def __init__(self, input_size, output_size, activator):
+    def __init__(self, input_size, output_size, input_activator, output_activator):
         '''
         全链接层初始化
 
@@ -14,7 +14,8 @@ class FullConnectedLayer(object):
         self.w_grad = np.zeros([output_size, input_size])
         self.b = np.zeros([output_size, 1])
         self.b_grad = np.zeros([output_size, 1])
-        self.activator = activator
+        self.input_activator = input_activator
+        self.output_activator = output_activator
         self.output = np.zeros([output_size, 1])
         self.delta = np.zeros([input_size, 1])
         self.input = np.zeros([input_size, 1])
@@ -22,17 +23,16 @@ class FullConnectedLayer(object):
     def forward(self, input_array):
         self.input = input_array
         z = np.dot(self.w, input_array) + self.b
-        self.output = self.activator.forward(z)
+        self.output = self.output_activator.forward(z)
 
     def backward(self, delta_array):
-        self.delta = self.activator.backward(self.input) * np.dot(self.w.T, delta_array)
+        self.delta = self.input_activator.backward(self.input) * np.dot(self.w.T, delta_array)
         self.w_grad = np.dot(delta_array, self.input.T)
         self.b_grad = delta_array
 
     def update(self, learning_rate):
         self.w += learning_rate * self.w_grad
         self.b += learning_rate * self.b_grad
-
 
 class SigmoidActivator(object):
     def forward(self, weighted_input):
@@ -42,11 +42,22 @@ class SigmoidActivator(object):
         return output * (1.0 - output)
 
 
+class IdentityActivator(object):
+    def forward(self, weighted_input):
+        return np.array(weighted_input)
+
+    def backward(self, output):
+        return 1
+
+
 class Network(object):
-    def __init__(self, layers):
+    def __init__(self, layers, output_activator):
         self.layers = []
         for i in range(layers.length - 1):
-            self.layers.append(FullConnectedLayer(layers[i], layers[i + 1], SigmoidActivator()))
+            if i == layers.length - 2:
+                self.layers.append(FullConnectedLayer(layers[i], layers[i + 1], SigmoidActivator(), output_activator))
+            else:
+                self.layers.append(FullConnectedLayer(layers[i], layers[i + 1], SigmoidActivator(), SigmoidActivator()))
 
     def forward(self, input_vec):
         for layer in self.layers:
@@ -54,7 +65,7 @@ class Network(object):
 
     def backward(self, label):
         predict = self.layers[-1].output
-        delta = self.layers[-1].activator.backward(predict) * (label - predict)
+        delta = self.layers[-1].output_activator.backward(predict) * (label - predict)
         for layer in self.layers[::-1]:
             layer.backward(delta)
             delta = layer.delta
@@ -78,6 +89,27 @@ class Network(object):
         return self.layers[-1].output
 
 
+def single_fc_layer_gradient_check():
+    error_fuction = lambda output: output.sum()
+    input = np.array([1, 2, 3, 4, 4])
+    delta = np.ones(3)
+    layer = FullConnectedLayer(5, 3, SigmoidActivator(), IdentityActivator())
+    layer.forward(input)
+    layer.backward(delta)
+    epsilon = 0.0001
+
+    for i in range(layer.w.shape[0]):
+        for j in range(layer.w.shape[1]):
+            layer.w[i][j] += epsilon
+            layer.forward(input)
+            error1 = error_fuction(layer.output)
+            layer.w[i][j] -= 2 * epsilon
+            layer.forward(input)
+            error2 = error_fuction(layer.output)
+            expected_gradient = (error1 - error2) / (2 * epsilon)
+            print('expected gradient: %f, actual: %f' % (expected_gradient, layer.w[i][j]))
+
+
 def gradient_check(network, sample_feature, sample_label):
     layer_index = np.random.randint(0, network.layers.length)
     layer = network.layers[layer_index]
@@ -99,3 +131,12 @@ def gradient_check(network, sample_feature, sample_label):
     error2 = network_error(network.predict(sample_feature), sample_label)
     expected_gradient = (error1 - error2) / (2 * epsilon)
     print('expected gradient: \t%f\nactual gradient:\t%f\n' % (expected_gradient, actual_gradient))
+
+
+if __name__ == '__main__':
+    a = np.array([1, 2, 3, 4, 4, 5]).reshape([3, 2])
+    b = np.array([1, 2, 3]).reshape([1, 3])
+    print(b.shape)
+    print(a.shape)
+    print(np.matmul(b, a))
+    # single_fc_layer_gradient_check()
