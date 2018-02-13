b4586977a70328928d63349ec0631b7b6edabc7e
shellhue <zeyu_huang@163.com>
2018-02-13 16:35:30 +0800

    LSTM梯度检查有问题但不知错在哪儿

--- lstm.py
+++ lstm.py
@@ -27,27 +27,23 @@ class LstmLayer(object):
         self.o_list = self.init_state_vec()
         self.ct_list = self.init_state_vec()
 
-        self.wfh, self.wfx, self.wfb = self.init_weights_mat()
-        self.wih, self.wix, self.wib = self.init_weights_mat()
-        self.woh, self.wox, self.wob = self.init_weights_mat()
-        self.wch, self.wcx, self.wcb = self.init_weights_mat()
-
-        self.wfh_grad, self.wfx_grad, self.wfb_grad = self.init_grad_mat()
-        self.wih_grad, self.wix_grad, self.wib_grad = self.init_grad_mat()
-        self.woh_grad, self.wox_grad, self.wob_grad = self.init_grad_mat()
-        self.wch_grad, self.wcx_grad, self.wcb_grad = self.init_grad_mat()
+        self.wfh, self.wfx, self.bf = self.init_weights_mat()
+        self.wih, self.wix, self.bi = self.init_weights_mat()
+        self.woh, self.wox, self.bo = self.init_weights_mat()
+        self.wch, self.wcx, self.bc = self.init_weights_mat()
 
         self.times = 0
 
         self.input_list = []
+        self.input_list.append(np.zeros([self.input_size, 1]))
 
     def init_state_vec(self):
         state_list = [np.zeros([self.output_size, 1])]
         return state_list
 
     def init_weights_mat(self):
-        mat_h = np.random.uniform(-1e-4, 1e-4, [self.output_size, self.output_size])
-        mat_x = np.random.uniform(-1e-4, 1e-4, [self.output_size, self.input_size])
+        mat_h = np.random.uniform(-1e-1, 1e-1, [self.output_size, self.output_size])
+        mat_x = np.random.uniform(-1e-1, 1e-1, [self.output_size, self.input_size])
         b = np.zeros([self.output_size, 1])
         return mat_h, mat_x, b
 
@@ -60,10 +56,10 @@ class LstmLayer(object):
     def forward(self, input_array):
         self.input_list.append(input_array)
         self.times += 1
-        f_t = self.calc_gate(input_array, self.wfh, self.wfx, self.wfb, self.gate_activator)
-        i_t = self.calc_gate(input_array, self.wih, self.wix, self.wib, self.gate_activator)
-        o_t = self.calc_gate(input_array, self.woh, self.wox, self.wob, self.gate_activator)
-        ct_t = self.calc_gate(input_array, self.wch, self.wcx, self.wcb, self.output_activator)
+        f_t = self.calc_gate(input_array, self.wfh, self.wfx, self.bf, self.gate_activator)
+        i_t = self.calc_gate(input_array, self.wih, self.wix, self.bi, self.gate_activator)
+        o_t = self.calc_gate(input_array, self.woh, self.wox, self.bo, self.gate_activator)
+        ct_t = self.calc_gate(input_array, self.wch, self.wcx, self.bc, self.output_activator)
 
         c_t = f_t * self.c_list[-1] + i_t * ct_t
 
@@ -106,10 +102,10 @@ class LstmLayer(object):
             delta_o_t = delta_h_t * tanh_c * self.gate_activator.backward(o_t)
             delta_i_t = delta_h_t * o_t * (1 - tanh_c * tanh_c) * ct_t * self.gate_activator.backward(i_t)
             delta_f_t = delta_h_t * o_t * (1 - tanh_c * tanh_c) * c_t_previous * self.gate_activator.backward(f_t)
-            delta_ct_t = delta_h_t * o_t * (1 - tanh_c * tanh_c) * i_t * (1 - ct_t * ct_t)
+            delta_ct_t = delta_h_t * o_t * (1 - tanh_c * tanh_c) * i_t * self.output_activator.backward(ct_t)
 
-            delta_h_t_previous = np.dot(self.woh.T, delta_o_t) + np.dot(self.wfh, delta_f_t) + \
-                                 np.dot(self.wih, delta_i_t) + np.dot(self.wch, delta_ct_t)
+            delta_h_t_previous = np.dot(self.woh.T, delta_o_t) + np.dot(self.wfh.T, delta_f_t) + \
+                                 np.dot(self.wih.T, delta_i_t) + np.dot(self.wch.T, delta_ct_t)
 
             delta_o_list[t] = delta_o_t
             delta_i_list[t] = delta_i_t
@@ -118,18 +114,10 @@ class LstmLayer(object):
             delta_h_list[t - 1] = delta_h_t_previous
 
         # 计算梯度
-        wfh_grad_list = []
-        wfx_grad_list = []
-        wfb_grad_list = []
-        wih_grad_list = []
-        wix_grad_list = []
-        wib_grad_list = []
-        woh_grad_list = []
-        wox_grad_list = []
-        wob_grad_list = []
-        wch_grad_list = []
-        wcx_grad_list = []
-        wcb_grad_list = []
+        self.wfh_grad, self.wfx_grad, self.bf_grad = self.init_grad_mat()
+        self.wih_grad, self.wix_grad, self.bi_grad = self.init_grad_mat()
+        self.woh_grad, self.wox_grad, self.bo_grad = self.init_grad_mat()
+        self.wch_grad, self.wcx_grad, self.bc_grad = self.init_grad_mat()
 
         for t in range(self.times, 0, -1):
             delta_o_t = delta_o_list[t]
@@ -139,31 +127,18 @@ class LstmLayer(object):
             h_t_previous = self.h_list[t - 1]
             input_t = self.input_list[t]
 
-            wfh_grad_list.append(np.dot(delta_f_t, h_t_previous.T))
-            wfx_grad_list.append(np.dot(delta_f_t, input_t.T))
-            wfb_grad_list.append(delta_f_t)
-            wih_grad_list.append(np.dot(delta_i_t, h_t_previous.T))
-            wix_grad_list.append(np.dot(delta_i_t, input_t.T))
-            wib_grad_list.append(delta_i_t)
-            woh_grad_list.append(np.dot(delta_o_t, h_t_previous.T))
-            wox_grad_list.append(np.dot(delta_o_t, input_t.T))
-            wob_grad_list.append(delta_o_t)
-            wch_grad_list.append(np.dot(delta_ct_t, h_t_previous.T))
-            wcx_grad_list.append(np.dot(delta_ct_t, input_t.T))
-            wcb_grad_list.append(delta_ct_t)
-
-        self.wfh_grad = reduce(lambda m, n: n + m, wfh_grad_list, np.zeros(self.wfh_grad.shape))
-        self.wfx_grad = reduce(lambda m, n: n + m, wfx_grad_list, np.zeros(self.wfx_grad.shape))
-        self.wfb_grad = reduce(lambda m, n: n + m, wfb_grad_list, np.zeros(self.wfb_grad.shape))
-        self.wih_grad = reduce(lambda m, n: n + m, wih_grad_list, np.zeros(self.wih_grad.shape))
-        self.wix_grad = reduce(lambda m, n: n + m, wix_grad_list, np.zeros(self.wix_grad.shape))
-        self.wib_grad = reduce(lambda m, n: n + m, wib_grad_list, np.zeros(self.wib_grad.shape))
-        self.woh_grad = reduce(lambda m, n: n + m, woh_grad_list, np.zeros(self.woh_grad.shape))
-        self.wox_grad = reduce(lambda m, n: n + m, wox_grad_list, np.zeros(self.wox_grad.shape))
-        self.wob_grad = reduce(lambda m, n: n + m, wob_grad_list, np.zeros(self.wob_grad.shape))
-        self.wch_grad = reduce(lambda m, n: n + m, wch_grad_list, np.zeros(self.wch_grad.shape))
-        self.wcx_grad = reduce(lambda m, n: n + m, wcx_grad_list, np.zeros(self.wcx_grad.shape))
-        self.wcb_grad = reduce(lambda m, n: n + m, wcb_grad_list, np.zeros(self.wcb_grad.shape))
+            self.wfh_grad += np.dot(delta_f_t, h_t_previous.T)
+            self.wfx_grad += np.dot(delta_f_t, input_t.T)
+            self.bf_grad += delta_f_t
+            self.wih_grad += np.dot(delta_i_t, h_t_previous.T)
+            self.wix_grad += np.dot(delta_i_t, input_t.T)
+            self.bi_grad += delta_i_t
+            self.woh_grad += np.dot(delta_o_t, h_t_previous.T)
+            self.wox_grad += np.dot(delta_o_t, input_t.T)
+            self.bo_grad += delta_o_t
+            self.wch_grad += np.dot(delta_ct_t, h_t_previous.T)
+            self.wcx_grad += np.dot(delta_ct_t, input_t.T)
+            self.bc_grad += delta_ct_t
 
     def init_delta(self):
         delta_list = []
@@ -175,19 +150,64 @@ class LstmLayer(object):
     def update(self, learning_rate):
         self.wfh -= learning_rate * self.wfh_grad
         self.wfx -= learning_rate * self.wfx_grad
-        self.wfb -= learning_rate * self.wfb_grad
+        self.bf -= learning_rate * self.bf_grad
         self.wih -= learning_rate * self.wih_grad
         self.wix -= learning_rate * self.wix_grad
-        self.wib -= learning_rate * self.wib_grad
+        self.bi -= learning_rate * self.bi_grad
         self.woh -= learning_rate * self.woh_grad
         self.wox -= learning_rate * self.wox_grad
-        self.wob -= learning_rate * self.wob_grad
+        self.bo -= learning_rate * self.bo_grad
         self.wch -= learning_rate * self.wch_grad
         self.wcx -= learning_rate * self.wcx_grad
-        self.wcb -= learning_rate * self.wcb_grad
+        self.bc -= learning_rate * self.bc_grad
+
+    def reset_state(self):
+        self.times = 0
+        self.c_list = self.init_state_vec()
+        self.h_list = self.init_state_vec()
+        self.f_list = self.init_state_vec()
+        self.i_list = self.init_state_vec()
+        self.o_list = self.init_state_vec()
+        self.ct_list = self.init_state_vec()
+        self.input_list = []
+        self.input_list.append(np.zeros([self.input_size, 1]))
+
+
+def lstm_gradient_check():
+
+    def error_function(x):
+        return x.sum()
+
+    x1 = np.array([[2], [4], [10], [10], [10]])
+    x2 = np.array([[8], [6], [5], [10], [10]])
+    x3 = np.array([[18], [13], [15], [10], [10]])
+    delta_array = np.array([[1], [1], [1]])
+
+    lstm = LstmLayer(5, 3, SigmoidActivator())
+    lstm.forward(x1)
+    lstm.forward(x2)
+    lstm.forward(x3)
+    lstm.backward(delta_array)
+
+    epsilon = 0.0001
+    for i in range(lstm.wfh.shape[0]):
+        for j in range(lstm.wfh.shape[1]):
+            lstm.reset_state()
+            lstm.wfh[i, j] += epsilon
+            lstm.forward(x1)
+            lstm.forward(x2)
+            lstm.forward(x3)
+            err1 = error_function(lstm.h_list[-1])
+            lstm.reset_state()
+            lstm.wfh[i, j] -= epsilon * 2
+            lstm.forward(x1)
+            lstm.forward(x2)
+            lstm.forward(x3)
+            err2 = error_function(lstm.h_list[-1])
+            expected_grad = (err1 - err2) / (2 * epsilon)
+            lstm.wfh[i, j] += epsilon
+            print('expected wfh grad: ', expected_grad, ' actual wfh grad: ', lstm.wfh_grad[i, j])
 
 
 if __name__ == '__main__':
-    a = np.array([[1, 2, 3, 5], [1, 2, 3, 8]])
-    np.insert(a, 0, [1, 2, 3, 5])
-    print(a)
+    lstm_gradient_check()
